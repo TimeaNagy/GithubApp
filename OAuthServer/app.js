21bcdd1c2c909ce4c0f25c5e1772e0acc04f4f78
TimeaNagy <nagytimea12@yahoo.com>
2021-05-30 14:53:32 +0200

    changed port

--- OAuthServer/app.js
+++ OAuthServer/app.js
@@ -42,7 +42,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(8080,function(){
+app.listen(process.env.PORT || 8080,function(){
   console.log('app listening on port 8080');
   })
 
